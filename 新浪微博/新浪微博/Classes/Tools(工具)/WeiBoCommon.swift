cca0171b7a529d4b31e47be3d5408648b8643702
YouareMylovelyGirl <mr_koreyoshi@icloud.com>
2017-06-08 00:00:34 +0800

    通过通知将用户登录需求传递给Main控制器

---
+++ 新浪微博/新浪微博/Classes/Tools(工具)/WeiBoCommon.swift
@@ -0,0 +1,12 @@
+//
+//  WeiBoCommon.swift
+//  新浪微博
+//
+//  Created by 阳光 on 2017/6/7.
+//  Copyright © 2017年 YG. All rights reserved.
+//
+
+import Foundation
+//MARK: - 全局通知定义
+//用户需要登录通知
+let UserShouldLoginNotification = "UserShouldLoginNotification"
