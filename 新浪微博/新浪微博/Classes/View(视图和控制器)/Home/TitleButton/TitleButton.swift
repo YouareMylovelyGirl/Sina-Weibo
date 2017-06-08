e8da285807e0af6faa8c73067d769f66c99541e8
阳光 <koreyoshi@cwp.local>
2017-06-08 19:26:33 +0800

    抽取button前

---
+++ 新浪微博/新浪微博/Classes/View(视图和控制器)/Home/TitleButton/TitleButton.swift
@@ -0,0 +1,21 @@
+//
+//  TitleButton.swift
+//  新浪微博
+//
+//  Created by 阳光 on 2017/6/8.
+//  Copyright © 2017年 YG. All rights reserved.
+//
+
+import UIKit
+
+class TitleButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
