5bfdf734dc90bdc7d8b9350546ac9d7c909179b4
阳光 <koreyoshi@cwp.local>
2017-06-08 10:52:32 +0800

    增加指示器, 和webview禁止滚动

--- 新浪微博/新浪微博/Classes/View(视图和控制器)/Main/OAuth/WBOAuthViewController.swift
+++ 新浪微博/新浪微博/Classes/View(视图和控制器)/Main/OAuth/WBOAuthViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SVProgressHUD
 /// 通过webView加载新浪微博授权页面控制器
 class WBOAuthViewController: UIViewController {
 
@@ -15,6 +16,10 @@ class WBOAuthViewController: UIViewController {
     override func loadView() {
         view = webView
         view.backgroundColor = UIColor.white
+        //取消滚动 - 新浪微博服务器, 返回的授权页面默认就是全屏
+        webView.scrollView.isScrollEnabled = false
+        
+        
         
         //设置导航栏
         title = "登录新浪微博"
@@ -46,7 +51,7 @@ class WBOAuthViewController: UIViewController {
     //点击登录按钮执行submit, 将本地的数据提交给服务器
     @objc private func autoFill() {
         //准备 js
-        let js = "document.getElementById('userId').value = '18710285537';" + "document.getElementById('passwd').value = 'person';"
+        let js = "document.getElementById('userId').value = '18710285537';" + "document.getElementById('passwd').value = 'personal';"
         //让webView直行js
         webView.stringByEvaluatingJavaScript(from: js)
     }
@@ -56,7 +61,9 @@ class WBOAuthViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @objc private func closeLoginView() {
+    @objc fileprivate func closeLoginView() {
+        //关闭菊花
+        SVProgressHUD.dismiss()
         dismiss(animated: true, completion: nil)
     }
     
@@ -86,11 +93,42 @@ extension WBOAuthViewController: UIWebViewDelegate {
         
         //确认思路: 
         //1.如果请求地址包含 http://baidu.com 不加载页面/ 否则加载页面
+        if request.url?.absoluteString.hasPrefix(RedirectURI) == false {
+           return true
+        }
+        
+        
         //2.从http://baidu.com 回调地址的'查询字符串' 中查找 'code='
         //如果有授权成功, 否则, 授权失败
+        if request.url?.query?.hasPrefix("code=") == false {
+            print("取消授权")
+            closeLoginView()
+            
+            return false
+        }
         
+        //3.从 query 字符串中取出授权码
+        //代码走到此处, url中一定有查®询字符串, 包含'code'
+        //"code=".endIndex 可以获取字符串的最后一个字符
+        let index = "code=".endIndex
+        let code = request.url?.query?.substring(from: index) ?? ""
         
-        print("加载请求--\(String(describing: request.url?.absoluteString))")
-        return true
+        print("获取授权码--\(String(describing: code))")
+        
+//        print("加载请求--\(String(describing: request.url?.absoluteString))")
+//        //query 就是url中 '?'后面的部分
+//        print("加载请求--\(String(describing: request.url?.query))")
+        return false
     }
+    //开始加载网页
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        //展示hud
+        SVProgressHUD.show()
+    }
+    //加载结束网页
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        //取消hud
+        SVProgressHUD.dismiss()
+    }
+    
 }
