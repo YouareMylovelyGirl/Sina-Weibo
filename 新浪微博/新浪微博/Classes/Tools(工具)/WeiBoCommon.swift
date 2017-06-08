4abfaaa9b195930b9e5ca10fbc2fa02d370bf71c
YouareMylovelyGirl <mr_koreyoshi@icloud.com>
2017-06-08 10:52:32 +0800

    增加指示器, 和webview禁止滚动

--- 新浪微博/新浪微博/Classes/Tools(工具)/WeiBoCommon.swift
+++ 新浪微博/新浪微博/Classes/Tools(工具)/WeiBoCommon.swift
@@ -14,7 +14,7 @@ let APPKey = "1659422547"
 //应用程序加密信息
 let APPSecret = "470bfdc420e5cdbcca12a60ffffba2da"
 //回调地址 - 登录完成跳转的URL, 参数以get形式拼接
-let RedirectURI = "http://www.baidu.com"
+let RedirectURI = "http://baidu.com"
 
 
 //MARK: - 全局通知定义
