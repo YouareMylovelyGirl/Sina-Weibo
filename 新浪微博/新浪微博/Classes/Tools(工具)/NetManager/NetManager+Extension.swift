0a42b904a460471deb6607583cf5c786fb758fb8
阳光 <koreyoshi@cwp.local>
2017-06-08 09:01:39 +0800

    添加应用程序信息

--- 新浪微博/新浪微博/Classes/Tools(工具)/NetManager/NetManager+Extension.swift
+++ 新浪微博/新浪微博/Classes/Tools(工具)/NetManager/NetManager+Extension.swift
@@ -13,7 +13,7 @@ extension NetManager {
     /// 加载微博字典数据
     ///   - since_id: 则返回ID比since_id大的微博（即比since_id时间晚的微博），默认为0。
     ///   - max_id: 则返回ID小于或等于max_id的微博，默认为0。
-    /// - Parameter completionHandler: 完成回调   [list: 微博字典数组/错误信息]
+    ///   - Parameter completionHandler: 完成回调   [list: 微博字典数组/错误信息]
 
     func stausList( since_id: Int64 = 0, max_id: Int64 = 0, completionHandler: @escaping (_ list: [[String: AnyObject]]?, _ error : NSError? ) -> ()) {
         //用AFN加载网络数据
--- 新浪微博/新浪微博/Classes/Tools(工具)/WeiBoCommon.swift
+++ 新浪微博/新浪微博/Classes/Tools(工具)/WeiBoCommon.swift
@@ -7,6 +7,16 @@
 //
 
 import Foundation
+
+//MARK: - 应用程序信息
+//应用程序ID
+let APPKey = "1659422547"
+//应用程序加密信息
+let APPSecret = "470bfdc420e5cdbcca12a60ffffba2da"
+//回调地址 - 登录完成跳转的URL, 参数以get形式拼接
+let RedirectURI = "http://www.baidu.com"
+
+
 //MARK: - 全局通知定义
 //用户需要登录通知
 let UserShouldLoginNotification = "UserShouldLoginNotification"
