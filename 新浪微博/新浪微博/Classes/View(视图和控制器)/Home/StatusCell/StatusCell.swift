9cd71aaa004de6b9372d07742a1875e6b93c9ab2
YouareMylovelyGirl <mr_koreyoshi@icloud.com>
2017-06-09 18:37:58 +0800

    添加微博cell的xib并且验证

---
+++ 新浪微博/新浪微博/Classes/View(视图和控制器)/Home/StatusCell/StatusCell.swift
@@ -0,0 +1,24 @@
+//
+//  StatusCell.swift
+//  新浪微博
+//
+//  Created by 阳光 on 2017/6/9.
+//  Copyright © 2017年 YG. All rights reserved.
+//
+
+import UIKit
+
+class StatusCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
