92a3f62dae4c7b98f3d59b0905ac9293f7c69c37
YouareMylovelyGirl <mr_koreyoshi@icloud.com>
2017-06-13 15:59:40 +0800

    刷新控件完成

--- 新浪微博/新浪微博/Classes/Tools(工具)/RefreshControl/MTRefreshView.swift
+++ 新浪微博/新浪微博/Classes/Tools(工具)/RefreshControl/MTRefreshView.swift
@@ -16,6 +16,26 @@ class MTRefreshView: RefreshView {
 
     @IBOutlet weak var earthIconView: UIImageView!
     
+    override var parentViewHeight: CGFloat {
+        didSet {
+            print("父视图\(parentViewHeight)")
+            if parentViewHeight < 50 {
+                return
+            }
+            var scale: CGFloat
+            if parentViewHeight > 126 {
+                scale = 1
+            } else {
+                
+                //高度差 / 最大高度差
+                 scale = 1 - ((126 - parentViewHeight) / (126 - 50))
+            }
+            
+            kangarooIconView.transform = CGAffineTransform(scaleX: scale, y: scale)
+            
+        }
+    }
+    
     override func awakeFromNib() {
         //1. 房子
         let bImage1 = #imageLiteral(resourceName: "icon_building_loading_1")
@@ -37,5 +57,13 @@ class MTRefreshView: RefreshView {
         let cImage2 = #imageLiteral(resourceName: "icon_small_kangaroo_loading_2")
         //两张图换来换去
         kangarooIconView.image = UIImage.animatedImage(with: [cImage1, cImage2], duration: 0.3)
+        
+        kangarooIconView.transform = CGAffineTransform(scaleX: 0.2, y: 0.2)
+        //1. 设置锚点
+        kangarooIconView.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
+        //2. 设置center
+        let x = self.bounds.width * 0.5
+        let y = self.bounds.height - 50
+        kangarooIconView.center = CGPoint(x: x, y: y)
     }
 }
